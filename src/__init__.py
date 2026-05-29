baaefcb2b95e89309e2d04f3c155aba4b3ecaa22
JesusTheHacker <skylerhend@gmail.com>
2026-05-29 05:53:00 -0500

    Enhance __init__.py with documentation and usage
    Added module docstring and usage instructions.

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,25 @@
-Python 2.7.18 (v2.7.18:8d21aa21f2, Apr 20 2020, 13:19:08) [MSC v.1500 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> # src/__init__.py
+"""
+VectorZip Engine
+----------------
+A high-performance hybrid compression library.
+Combines LZ77 sliding window dictionary matching with Huffman entropy coding.
+
+Usage:
+    from VectorZip import compress_file
+
+Attributes:
+    __version__ (str): 1.0.0
+"""
+
 from .lz77 import lz77_compress
 from .huffman import huffman_encode
 from .compressor import compress_file
 
-# Define what gets imported with "from src import *"
-__all__ = ['lz77_compress', 'huffman_encode', 'compress_file']
+# Public API Surface
+__all__ = [
+    'lz77_compress', 
+    'huffman_encode', 
+    'compress_file'
+]
 
-# Package version
 __version__ = '1.0.0'
