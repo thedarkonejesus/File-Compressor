792d4db18780d65b6417fc632278e5fe64c23f95
JesusTheHacker <skylerhend@gmail.com>
2026-05-29 06:24:15 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,22 @@ from setuptools import setup, find_packages
 setup(
     name="VectorZip",
     version="1.0.0",
+    description="A high-performance, chunked-processing compression utility.",
+    author="VectorZip Developer",
     packages=find_packages(),
-    install_requires=[],  # Add dependencies like 'pytest' here if needed
-    author="Your Name",
-    description="A high-performance LZ77 + Huffman compression engine",
+    install_requires=[
+        "tqdm>=4.60.0",
+    ],
     entry_points={
         'console_scripts': [
             'vzip=main:main',
         ],
     },
+    python_requires='>=2.7',
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Operating System :: OS Independent",
+    ],
 )
